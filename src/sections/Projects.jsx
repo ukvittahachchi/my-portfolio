bfbd2fdf0d26ade839d83b7fe2a1932d996e8aa3
ukvittahachchi <ukvittahachchi99@gmail.com>
2025-08-29 11:24:20 +0530

    project

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,9 +1,9 @@
 // src/sections/Projects.jsx
 import React, { useState, useEffect, useRef } from "react";
 import ProjectCard from "../components/ProjectCard";
-import project1 from "../assets/project1.png";
-import project2 from "../assets/project2.png";
-import project3 from "../assets/project3.png";
+import project1 from "../assets/Project1.png";
+import project2 from "../assets/Project2.png";
+import project3 from "../assets/Project3.png";
 import { motion } from "framer-motion";
 
 const Projects = () => {
